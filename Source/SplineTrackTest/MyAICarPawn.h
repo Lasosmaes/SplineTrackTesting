727ffafb769adc19a2aff47ba0470e6d9d315702
Cameron <cm1crt@bolton.ac.uk>
2019-02-28 21:27:11 +0000

    AI Follows AISpline Track

---
+++ Source/SplineTrackTest/MyAICarPawn.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "MyAICarPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLINETRACKTEST_API AMyAICarPawn : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
